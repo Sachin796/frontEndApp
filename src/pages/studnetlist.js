c0dfa0ea1a3499bc3c397d4bec029407a80a99ef
Sachin796 <sachinjaveri.2010@gmail.com>
2020-07-17 19:47:00 -0400

    FrontEnd

---
+++ src/pages/studnetlist.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Details from "../components/details";
+class StudentListPage extends Component {
+  state = {
+    studentList: null,
+  };
+
+  componentDidMount() {
+    fetch("https://codingtest-server.herokuapp.com/api/students")
+      .then((res) => res.json())
+      .then((data) => this.setState({ studentList: data }));
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        {this.state.studentList != null ? (
+          <>
+            <div
+              style={{ marginTop: "30px", marginLeft: "40%", width: "100%" }}
+            >
+              <table>
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>Email</th>
+                  <th>City</th>
+                </tr>
+                <tbody>
+                  {this.state.studentList.data.map((student) => (
+                    <>
+                      <tr>
+                        <Details student={student} />
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
+        ) : null}
+
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default StudentListPage;
